1c06f440d5672ee4a93527da0c86e107e21a8086
DamianRiella <40808632+DamianRiella@users.noreply.github.com>
2018-09-03 21:54:34 -0300

    Add files via upload

---
+++ RIELLA/Calculadora/funciones.c
@@ -0,0 +1,40 @@
+/* Funciones de SUMA, RESTA, MULTIPLICACION, DIVICION, FACTORIAL*/
+
+int factorial(int num)
+{
+    int valor, resultado=1, i;
+
+    for(i=valor; i>0; i--)
+    {
+        resultado= resultado*i;
+        return resultado;
+    }
+
+}
+
+float suma(float num1, float num2)
+{
+    int resultado;
+    resultado= num1 + num2;
+    return resultado;
+}
+
+float resta(float num1, float num2)
+{
+    int resultado;
+    resultado= num1 - num2;
+    return resultado;
+}
+
+float multiplicacion(float num1, float num2)
+{
+    int resultado;
+    resultado= num1 * num2;
+    return resultado;
+}
+float division(float num1, float num2)
+{
+    int resultado;
+    resultado= num1 / num2;
+    return resultado;
+}
---
+++ RIELLA/Calculadora/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float suma(float, float);
+float resta(float, float);
+float multiplicacion(float, float);
+float divicion(float, float);
+int factorial(int);
+
+#endif // FUNCIONES_H_INCLUDED
